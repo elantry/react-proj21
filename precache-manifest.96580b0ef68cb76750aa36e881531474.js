5b5d96e091cedb6446f081529d0eaa224f896aec
elantry <mohamedahmed25173@gmail.com>
2022-02-16 19:03:52 +0200

    Updates

---
+++ precache-manifest.96580b0ef68cb76750aa36e881531474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61920b90f16f09ea14ca72f8486c956f",
+    "url": "/react-proj21/index.html"
+  },
+  {
+    "revision": "6d67f21ac334c97c8589",
+    "url": "/react-proj21/static/css/main.35134b96.chunk.css"
+  },
+  {
+    "revision": "dc21e545a28f12e4ada5",
+    "url": "/react-proj21/static/js/2.aafdef60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-proj21/static/js/2.aafdef60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d67f21ac334c97c8589",
+    "url": "/react-proj21/static/js/main.f09ee12e.chunk.js"
+  },
+  {
+    "revision": "8b981c5af62c6c902bd3",
+    "url": "/react-proj21/static/js/runtime-main.f0823c5f.js"
+  }
+]);
